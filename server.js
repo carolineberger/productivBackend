da0c6a3deb194d92d6338a7bc324692f8c8a8c37
carolineberger <caroline.berger2@mail.mcgill.ca>
2018-06-25 16:34:16 +0200

    Backend git

---
+++ server.js
@@ -0,0 +1,36 @@
+
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 8080,
+mongoose = require('mongoose'),
+Activity = require('./models/activityModel'), 
+bodyParser = require('body-parser');
+
+var cors = require('cors')
+app.use(express.static('www'));
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./routes/activityRoutes');
+routes(app); 
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/activity' || 'mongodb://test_user:testuser1@ds161710.mlab.com:61710/productiv_activities', function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+ 
+
+
+
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
